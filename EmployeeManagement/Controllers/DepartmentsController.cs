cdc78b3920e98effb9e08cdfbb75925b90e42c16
Venkata Sunil Puchaginjala <sunil.puchaginjala@cgi.com>
2021-09-05 18:02:14 -0700

    DepartmentsController is not being used

--- EmployeeManagement/Controllers/DepartmentsController.cs
+++ EmployeeManagement/Controllers/DepartmentsController.cs
@@ -7,6 +7,7 @@ namespace EmployeeManagement.Controllers
 {
     public class DepartmentsController
     {
+        // not using this currently
         public string List()
         {
             return "List() of DepartmentsController";
